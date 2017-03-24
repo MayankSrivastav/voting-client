4e4bb450d4b5a947d3bedf9a92f3ae1ef88730d7
MayankSrivastav <mayanksri18@yahoo.in>
2017-03-24 20:54:21 +0530

    Updated comments

--- test/test_helper.js
+++ test/test_helper.js
@@ -5,5 +5,6 @@ import jsdom from 'jsdom';
 const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
 const win = doc.defaultView();
 
+// Set the global document as doc
 global.document = doc;
 global.window = win;
